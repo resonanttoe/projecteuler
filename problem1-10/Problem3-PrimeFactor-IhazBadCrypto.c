0ab9112877ac7f82f2aff32bfbf33d6693f0607f
Dev <github@muteTV.org>
2015-02-28 00:25:16 -0800

    Problem 3 - With assist for maths portions of it because my factoring sucked. Will revisit tomorrow and do blindly as well.

---
+++ problem1-10/Problem3-PrimeFactor-IhazBadCrypto.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int multiples()
+{
+	int multiple;
+	long factee = 600851475143;
+
+	for (multiple = 1; multiple < factee; multiple = multiple + 2) {
+	    if (multiple * multiple < factee) {
+		if (factee % multiple == 0) {
+			factee = factee / multiple;
+			}
+		multiple = multiple + 1;
+		}
+	}
+	return factee;
+}
+	
+
+int main (void)
+{
+	printf("%d\n", multiples());
+	return 0;
+}
+
